7b4dca41df9f5ee787b8d214601f65eba1cde716
Kelvin <115596454+JKelvin001@users.noreply.github.com>
2023-01-02 21:52:58 -0300

    up

---
+++ aula004/ex001.js
@@ -0,0 +1,7 @@
+var v = 98.7
+console.log(`A velocidade atual de seu carro é de ${v}Km/H`)
+if (v > 80){
+    console.log(`Você ultrapassou a velocidade permitida. Multa aplicada!`)
+}
+console.log(`Lembre-se de sempre usar o cinto de segurança!`)
+/*Condição Simples*/
---
+++ aula004/ex002.js
@@ -0,0 +1,7 @@
+var p = 'Brasil'
+console.log(`Vivendo no(a) ${p}`)
+if (p == 'Brasil'){
+    console.log(`Você é um Brasileiro!`)
+} else{
+    console.log(`Você é um estrangeiro!`)
+}
