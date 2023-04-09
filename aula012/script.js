c9e107d260a5778dacc55b049e86f391ef504206
Kelvin <115596454+JKelvin001@users.noreply.github.com>
2023-04-09 11:26:31 -0300

    att

--- aula012/script.js
+++ aula012/script.js
@@ -19,8 +19,40 @@ function inLista(n, l){
 }    
 function analize(){    
     if (isNumero(num.value) && !inLista(num.value, valor)){
-        
+         valor.push(Number(num.value))
+         let item = document.createElement('option')
+         item.text = `Valor ${num.value} adciionado.`
+         bd.appendChild(item) 
+         res.innerHTML = ''
     }else{
-        window.alert('[ERROR]')
+        window.alert('[Valor invalido pois já consta da lista.]')
     }
+    num.value = ''
+    num.focus()
+}
+function finalizar(){
+    if (valor.length == 0){
+        window.alert('Valores nulos para finalização')
+    } else{
+        let tot = valor.length
+        let maior = valor[0]
+        let menor = valor[0]
+        let soma = 0
+        let media = 0
+        for(let pos in valor) {
+            soma += valor[pos]
+            if (valor[pos] > maior)
+                maior = valor[pos]
+            if (valor[pos] < menor)
+                menor = valor[pos]    
+        }
+        media = soma / tot
+        res.innerHTML = ''
+        res.innerHTML += `<p> Ao todo, temos ${tot} números cadastrados.</p>`
+        res.innerHTML += `<p> O maior valor informado foi: ${maior}</p>`
+        res.innerHTML += `<p> O menor valor informado foi: ${menor}</p>`
+        res.innerHTML += `<p> Somando todos os valores, temos: ${soma}.</p>`
+        res.innerHTML += `<p> A media dos valores informados é: ${media}</p>`
+    }
+
 }
